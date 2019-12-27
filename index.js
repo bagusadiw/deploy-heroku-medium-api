f7c4de41cb20c4f904b9dca688bb2511660d2b5f
bagusadiw <bagusadi1494@gmail.com>
2019-12-27 13:45:25 +0700

    5th commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const express = require('express')
 const cors = require('cors')
 //use express in app variable
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 // jwt middleware
 const { authenticated } =require('./middleware')
 //import controllers
