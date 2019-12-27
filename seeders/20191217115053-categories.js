7c7c12cb4668504cb0d774f30e2e2f9bee7f8f55
bagusadiw <bagusadi1494@gmail.com>
2019-12-27 16:17:50 +0700

    11th commit

--- seeders/20191217115053-categories.js
+++ seeders/20191217115053-categories.js
@@ -5,41 +5,49 @@ module.exports = {
    return queryInterface.bulkInsert('categories', [
       {
         name: 'Home',
+        url: '/',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         name: 'Onezero',
+        url: '/category',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         name: 'Elemental',
+        url:'/article',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         name: 'Gen',
+        url: '/',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         name: 'Zora',
+        url: '/category',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         name: 'Forge',
+        url:'/article',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         name: 'Human Parts',
+        url: '/',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         name: 'Marker',
+        url:'/category',
         createdAt: new Date(),
         updatedAt: new Date()
       }      
